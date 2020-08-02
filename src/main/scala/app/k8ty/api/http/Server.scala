4861c7f1460398146289820d75a5ffcb9bad12d0
Mark Rudolph <mark@k8ty.app>
2020-08-01 22:33:51 -0400

    Tweaks for  graphiql.html

--- src/main/scala/app/k8ty/api/http/Server.scala
+++ src/main/scala/app/k8ty/api/http/Server.scala
@@ -48,7 +48,7 @@ object Server {
 
   private val originConfig = CORSConfig(
     anyOrigin = false,
-    allowedOrigins = Set("https://k8ty.app", "http://localhost:4200"),
+    allowedOrigins = Set("https://k8ty.app", "http://localhost:4200", "http://localhost:9000"),
     allowCredentials = false,
     maxAge = 1.day.toSeconds
   )
